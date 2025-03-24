5bc5860c44f8793a5f1f894749e0decde646ba8c
Aakash <aakashkumar2908@gmail.com>
2025-03-24 20:41:56 +0530

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import react from 'react';
+
+export default function Home() {
+  return (
+    <div className="">
+      <canvas className="w-full h-full" id="canvas"></canvas>
+    </div>
+  );
+}
